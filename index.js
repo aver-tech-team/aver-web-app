b8a84458586b6535feac60ff2af8672b269d815d
Samuel Johnson <email@example.com>
2023-11-15 11:53:13 -0600

    Added error page

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ app.use(cors())
 
 app.use(express.static(path.join(__dirname, '/dist')));
 
+app.get('/login', function(req, res){
+    res.status(404).sendFile(path.join(__dirname, "/dist/index.html"));
+  });  
+
+app.get('/signup', function(req, res){
+res.status(404).sendFile(path.join(__dirname, "/dist/index.html"));
+});  
+
+app.get('*', function(req, res){
+    res.status(404).sendFile(path.join(__dirname, "error.html"));
+  });  
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
